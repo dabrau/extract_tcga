a68496dff518896f0345d44870b3b3205d16edf8
Darrell Abrau <darrellabrau@DOM-APANZER-MAC.ucsf.edu>
2018-01-19 17:29:45 -0800

    Rename functions

--- clinical.R
+++ clinical.R
@@ -2,7 +2,7 @@ library(tidyverse)
 source("./utils.R")
 
 # get all clinical XML data as dataframes from TCGA by project
-GetClinical <- function(proj) {
+GetClinicalData <- function(proj) {
   QueryData(proj, "Clinical") %>%
     PrepareClinicalXMLData(c("admin", "patient", "stage_event", "radiation", "follow_up", "drug", "new_tumor_event"))
 }
